ad16725cb2cd2d4b250b1f395b4c3bcccd6d1403
Shannon Lewis <slewis74@gmail.com>
2016-10-19 09:47:00 +1000

    AssemblyInfo fix

--- source/Octopus.Data/Properties/AssemblyInfo.cs
+++ source/Octopus.Data/Properties/AssemblyInfo.cs
@@ -2,15 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Octopus.DataModelling")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Data interfaces for Octopus, an opinionated deployment solution for .NET applications")]
+[assembly: AssemblyCompany("Octopus Deploy")]
+[assembly: AssemblyProduct("Octopus, an opinionated deployment solution for .NET applications")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2016")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Octopus.Data")]
