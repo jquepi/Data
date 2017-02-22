63b585b3dd7792bd87f19a496925c2a07c013a20
Michael Noonan <michaelnoonan@gmail.com>
2017-02-22 19:01:14 +1000

    Fixed API to return both required outputs.

--- source/Octopus.Data/Storage/User/IUserStore.cs
+++ source/Octopus.Data/Storage/User/IUserStore.cs
@@ -11,7 +11,9 @@ namespace Octopus.Data.Storage.User
         IUser GetById(string userId);
         IUser GetByUsername(string username);
         IUser GetByIdentificationToken(Guid identificationToken);
+        [Obsolete("Please use the TryGetByApiKey() method which returns both the ApiKeyDescriptor and IUser.", true)]
         IUser GetByApiKey(string apiKey);
+        bool TryGetByApiKey(string apiKey, out ApiKeyDescriptor storedApiKey, out IUser user);
 
         UserCreateOrUpdateResult Create(string username, string displayName, string emailAddress, string externalId, ApiKeyDescriptor apiKeyDescriptor = null, string id = null, string password = null, bool isService = false, bool generateRandomPasswordOnCreate = false);
 
