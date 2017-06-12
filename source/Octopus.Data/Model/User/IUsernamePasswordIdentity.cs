5b55bdce5e5aff05634fc15fbf8fb70501eb0d5c
Shannon Lewis <slewis74@gmail.com>
2017-06-12 15:31:25 +1000

    param name change to make it clear that it's plain text

--- source/Octopus.Data/Model/User/IUsernamePasswordIdentity.cs
+++ source/Octopus.Data/Model/User/IUsernamePasswordIdentity.cs
@@ -2,7 +2,7 @@ namespace Octopus.Data.Model.User
 {
     public interface IUsernamePasswordIdentity : IIdentity
     {
-        void SetPassword(string password);
+        void SetPassword(string plainTextPassword);
         bool ValidatePassword(string plainTextPassword);
     }
 }
