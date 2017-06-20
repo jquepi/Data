760ae60d20794567dfe3d6e064593fb115f3ed40
Shannon Lewis <slewis74@gmail.com>
2017-06-20 13:48:53 +1000

    don't disclose how the security groups are being stored

--- source/Octopus.Data/Model/User/IUser.cs
+++ source/Octopus.Data/Model/User/IUser.cs
@@ -20,9 +20,8 @@ namespace Octopus.Data.Model.User
         
         HashSet<Identity> Identities { get; }
 
-        IDictionary<string, SecurityGroups> SecurityGroupIds { get; }
-        
         void ClearSecurityGroupIds(string provider);
+        SecurityGroups GetSecurityGroups(string provider);
         void SetSecurityGroupIds(string provider, IEnumerable<string> ids, DateTimeOffset updated);
         
         void SetPassword(string plainTextPassword);
