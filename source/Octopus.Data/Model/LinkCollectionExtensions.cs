6502a7e0f9a826761158646c00327945dbb4b229
Robert Wagner <robert.wagner@octopus.com>
2017-07-13 19:56:27 +1000

    Added `Page.All` and `Page.Last`

--- source/Octopus.Data/Model/LinkCollectionExtensions.cs
+++ source/Octopus.Data/Model/LinkCollectionExtensions.cs
@@ -6,6 +6,7 @@ namespace Octopus.Data.Model
     {
         public static LinkCollection Pagination(this LinkCollection collection, Func<int, int, string> formatWithSkipAndTake, int currentOffset, int perPage, int total)
         {
+            collection.Add("Page.All", formatWithSkipAndTake(0, int.MaxValue));
             if (currentOffset > 0)
             {
                 collection.Add("Page.Previous", formatWithSkipAndTake(Math.Max(currentOffset - perPage, 0), perPage));
@@ -17,6 +18,9 @@ namespace Octopus.Data.Model
             }
 
             collection.Add("Page.Current", formatWithSkipAndTake(currentOffset, perPage));
+
+            var lastPage = Math.Max(0, (int)Math.Ceiling((double)total / perPage) - 1);
+            collection.Add("Page.Last", formatWithSkipAndTake(lastPage * perPage, perPage));
 
             return collection;
         }
