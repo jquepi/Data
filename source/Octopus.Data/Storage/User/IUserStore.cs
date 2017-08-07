16729939851368e424f6678a0efb2be5d326ddf1
Shannon Lewis <slewis74@gmail.com>
2017-08-07 21:18:11 +1000

    fody fail

--- source/Octopus.Data/Storage/User/IUserStore.cs
+++ source/Octopus.Data/Storage/User/IUserStore.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Octopus.Data.Model.User;
 
 namespace Octopus.Data.Storage.User
@@ -24,22 +23,7 @@ namespace Octopus.Data.Storage.User
             string password,
             bool isService);
 
-        [ObsoleteEx(Message = "This method handles requesting the groups internally", TreatAsErrorFromVersion = "2.0", RemoveInVersion = "2.0")]
-        UserCreateOrUpdateResult CreateOrUpdate(
-            string username,
-            string displayName, 
-            string emailAddress, 
-            string externalId, 
-            ApiKeyDescriptor apiKeyDescriptor, 
-            bool generateRandomPasswordOnCreate, 
-            string password,
-            bool isService,
-            string[] externalGroups);
-
         void EnableUser(string userId);
         void DisableUser(string userId);
-
-        [ObsoleteEx(Message = "Groups are handled by the scheduler  internally", TreatAsErrorFromVersion = "2.0", RemoveInVersion = "2.0")]
-        void UpdateUsersExternalGroups(IUser user, HashSet<string> groups);
     }
 }
