291adc4c2c1e6097b1638d6095b6c772f75fd3c2
Andrew Katsivas <andrew.katsivas@gmail.com>
2017-10-05 17:48:54 +1030

    Migrating Resource and related types into Data, from Core

---
+++ source/Octopus.Data/Resources/IResource.cs
@@ -0,0 +1,21 @@
+﻿using Octopus.Data.Model;
+
+namespace Octopus.Data.Resources
+{
+    /// <summary>
+    /// Implemented by all resources.
+    /// </summary>
+    public interface IResource
+    {
+        /// <summary>
+        /// Gets a unique identifier for this resource.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Gets or sets a dictionary of links to other related resources. These links can be used to navigate the resources on
+        /// the server.
+        /// </summary>
+        LinkCollection Links { get; set; }
+    }
+}
