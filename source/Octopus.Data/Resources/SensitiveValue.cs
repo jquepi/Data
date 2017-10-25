2e7d5ffac980fe06ea4d72be6e6c4bfb071f98b0
Andrew Katsivas <andrew.katsivas@gmail.com>
2017-10-25 17:04:45 +1030

    Moved SensitiveValue here so extensions can use it for storing config

---
+++ source/Octopus.Data/Resources/SensitiveValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Octopus.Data.Resources
+{
+    public class SensitiveValue : IEquatable<SensitiveValue>
+    {
+        public bool HasValue { get; set; }
+        public string NewValue { get; set; }
+
+        public static implicit operator SensitiveValue(string newValue)
+        {
+            return new SensitiveValue { HasValue = newValue != null };
+        }
+
+        public bool Equals(SensitiveValue other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.HasValue, HasValue) && Equals(other.NewValue, NewValue);
+        }
+    }
+}
