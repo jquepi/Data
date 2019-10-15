43456742f6e07dd681356bb7ab797b3ddf54b785
Shannon Lewis <slewis74@gmail.com>
2019-10-15 10:25:37 +1000

    removing some attributes that got moved from OD, but probably shouldn't have

--- source/Octopus.Data/Resources/Attributes/ScriptConsoleSupportedAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Octopus.Data.Resources.Attributes
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public class ScriptConsoleSupportedAttribute : Attribute
-    {
-    }
-}
