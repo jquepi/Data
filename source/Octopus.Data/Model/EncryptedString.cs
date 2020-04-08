77cfdaa25445046c481fcd8897f7afa64737c068
Shannon Lewis <slewis74@gmail.com>
2020-04-08 17:34:15 +1000

    made EncryptedString IEquatable to itself and string

--- source/Octopus.Data/Model/EncryptedString.cs
+++ source/Octopus.Data/Model/EncryptedString.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Octopus.Data.Model
 {
-    public class EncryptedString
+    public class EncryptedString : IEquatable<EncryptedString>, IEquatable<string>
     {
         public EncryptedString()
         {}
@@ -11,5 +13,32 @@ namespace Octopus.Data.Model
         }
 
         public string Value { get; set; }
+
+        public bool Equals(EncryptedString other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value == other.Value;
+        }
+
+        public bool Equals(string other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((EncryptedString) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Value != null ? Value.GetHashCode() : 0);
+        }
     }
 }
