9b8ec50482da7c5b7d556ccd39891bad7dd8c27c
Shannon Lewis <slewis74@gmail.com>
2020-04-21 10:20:58 +1000

    user should be IId, not IDocument, it doesn't have a Name

--- source/Octopus.Data/Model/User/IUser.cs
+++ source/Octopus.Data/Model/User/IUser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Octopus.Data.Model.User
 {
-    public interface IUser : IDocument
+    public interface IUser : IId
     {
         string Username { get; }
         Guid IdentificationToken { get; }
