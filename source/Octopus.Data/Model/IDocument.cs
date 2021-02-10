308cf95c9b78d542c78c5b6596be9b17fc3c1db4
slewis74 <slewis74@gmail.com>
2021-02-10 11:48:02 +1000

    allow IDocument to define it's Id type

--- source/Octopus.Data/Model/IDocument.cs
+++ source/Octopus.Data/Model/IDocument.cs
@@ -1,8 +1,14 @@
 using System;
+using Octopus.TinyTypes;
 
 namespace Octopus.Data.Model
 {
     public interface IDocument : IId, INamed
+    {
+    }
+
+    public interface IDocument<out TId> : IId<TId>, INamed
+        where TId : CaseInsensitiveStringTinyType
     {
     }
 }
