524ca443f238e6b7c6bc43bc3ad272f4a4380580
slewis74 <slewis74@gmail.com>
2021-03-31 18:13:58 +1000

    moving message contracts out of server repo

--- source/Octopus.Data/Model/User/Identity.cs
+++ source/Octopus.Data/Model/User/Identity.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
-using Octopus.Data.Resources.Users;
 
 namespace Octopus.Data.Model.User
 {
-    public sealed class Identity : IEquatable<Identity>, IEquatable<IdentityResource>
+    public sealed class Identity : IEquatable<Identity>
     {
         public Identity(string identityProviderName)
         {
@@ -43,60 +42,6 @@ namespace Octopus.Data.Model.User
                         StringComparison.OrdinalIgnoreCase);
                 return bothClaimsAreNullOrWhitespace || existingClaimHasValueAndMatches;
             });
-        }
-
-        public bool Equals(IdentityResource other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (IdentityProviderName != other.IdentityProviderName) return false;
-            return Claims.All(kvp =>
-            {
-                if (!kvp.Value.IsIdentifyingClaim || kvp.Value.IsServerSideOnly)
-                    return true;
-                if (!other.Claims.ContainsKey(kvp.Key))
-                    return false;
-                var existingClaimValue = kvp.Value.Value;
-                var bothClaimsAreNullOrWhitespace = string.IsNullOrWhiteSpace(existingClaimValue) &&
-                    string.IsNullOrWhiteSpace(other.Claims[kvp.Key].Value);
-                var existingClaimHasValueAndMatches = !string.IsNullOrWhiteSpace(existingClaimValue) &&
-                    existingClaimValue.Equals(other.Claims[kvp.Key].Value,
-                        StringComparison.OrdinalIgnoreCase);
-                return bothClaimsAreNullOrWhitespace || existingClaimHasValueAndMatches;
-            });
-        }
-
-        public IdentityResource ToResource()
-        {
-            return new IdentityResource(IdentityProviderName)
-                .WithClaims(Claims);
-        }
-
-        internal Identity WithClaims(Dictionary<string, IdentityClaimResource> claims)
-        {
-            foreach (var kvp in claims)
-                if (Claims.ContainsKey(kvp.Key))
-                    Claims[kvp.Key].Value = kvp.Value.Value;
-                else
-                    Claims.Add(kvp.Key, new IdentityClaim(kvp.Value.Value, kvp.Value.IsIdentifyingClaim));
-            return this;
-        }
-
-        public Identity WithClaim(string type, string value, bool isIdentifyingClaim, bool isServerSideOnly = false)
-        {
-            if (Claims.ContainsKey(type))
-            {
-                var claim = Claims[type];
-                claim.Value = value;
-                claim.IsIdentifyingClaim = isIdentifyingClaim;
-                claim.IsServerSideOnly = isServerSideOnly;
-            }
-            else
-            {
-                var claim = new IdentityClaim(value, isIdentifyingClaim, isServerSideOnly);
-                Claims.Add(type, claim);
-            }
-
-            return this;
         }
 
         public override bool Equals(object obj)
